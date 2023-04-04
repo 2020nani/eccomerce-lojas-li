abc4971c40ad7e925c70b56e937f037696e11b80
2020nani <nanialmeida2019@gmail.com>
2023-04-04 14:59:49 -0300

    ajuste erro modal

--- doces-lu-cliente/src/app/carrinho/carrinho.module.ts
+++ doces-lu-cliente/src/app/carrinho/carrinho.module.ts
@@ -4,11 +4,9 @@ import { CarrinhoComponent } from './carrinho.component';
 import { FormsModule } from '@angular/forms';
 import { CarrinhoRoutingModule } from './carrinho.routing.module';
 import { ProdutosService } from '../shared/services/produtos.service';
-import { ModalLoadingComponent } from '../modal-loading/modal-loading.component';
-import { ErroComponent } from '../erro/erro.component';
 
 @NgModule({
-  declarations: [CarrinhoComponent, ErroComponent, ModalLoadingComponent],
+  declarations: [CarrinhoComponent],
   imports: [CommonModule, FormsModule, CarrinhoRoutingModule],
   providers: [ProdutosService],
 })
